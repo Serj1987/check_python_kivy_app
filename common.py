976578d95fe923ab4084de245bd8a68fbf855310
Sergey Tarasov <serj-mlp@mail.ru>
2021-12-07 11:00:06 +0300

    Update common.py
    add request of sqlite3 data in table in fourth screen in tableWindow

--- common.py
+++ common.py
@@ -1,18 +1,16 @@
-from kivy.app import App
+from kivy.uix.anchorlayout import AnchorLayout
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager, Screen  # , WipeTransition
 from kivy.core.window import Window
-from kivymd.uix.list import OneLineListItem
 from kivymd.uix.datatables import MDDataTable
 from kivy.metrics import dp
+import sqlite3
 
 Window.size = 375, 667
 
 
 class MainMenu(Screen):
-
-    def check(self):
-        print('просмотр данных')
+    pass
 
 
 class SecondScreen(Screen):
@@ -33,58 +31,58 @@ class Browse(Screen):
     def show_changes(self, chk_number, chk_date, chk_all):
 
         if chk_number:
-            # self.add_datatable()
             print(self.ids.input_det.text, 'number')
 
         if chk_date:
             print(self.ids.input_det.text, 'date')
 
-        # for i in range(20):
-        #    self.ids.container.add_widget(OneLineListItem(text=f"Single-line item {i}"))
-
         if chk_all:
             print(self.ids.input_det.text, 'all')
 
 
-class CommonApp(MDApp):
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+class TableWindow(Screen):
+    def __init__(self, **kw):
+        super().__init__(**kw)
         self.data_tables = None
+        self.cur = None
+        self.rows = None
+
+    def add_table(self):
+        self.con = sqlite3.connect('container.db')
+        self.cur = self.con.cursor()
+        self.cur.execute("SELECT * FROM details")
+        self.rows = self.cur.fetchall()
+
+        layout = AnchorLayout()
+        self.data_tables = MDDataTable(
+            use_pagination=True,
+            column_data=[
+                ("№ детали", dp(20)),
+                ("Наименование", dp(30)),
+                ("Количество", dp(15)),
+                ("Дата", dp(20)),
+                ("Примечание", dp(20)),
+                ("Метка", dp(30)),
+            ],
+            row_data=[self.row for self.row in self.rows],
+        )
+        self.add_widget(self.data_tables)
+        return layout
+
+    def on_enter(self):
+        self.add_table()
+
+
+class CommonApp(MDApp):
 
     def build(self):
         sm = ScreenManager()  # transition=WipeTransition())
         sm.add_widget(MainMenu(name='main_menu'))
         sm.add_widget(SecondScreen(name='second_screen'))
         sm.add_widget(Browse(name='browse'))
-
+        sm.add_widget(TableWindow(name='table'))
         return sm
 
-
-'''
-    def add_datatable(self): # need to add a table in browse screen
-        self.data_tables = MDDataTable(
-            size_hint=(0.9, 0.8),
-            column_data=[
-                ("No.", dp(30)),
-                ("User", dp(30)),
-                ("Password", dp(30)),
-            ],
-            row_data=[
-                (
-                    "1",
-                    "The pitonist",
-                    "Strong password",
-                ),
-                (
-                    "2",
-                    "The c++ lover",
-                    "Save me!!!:)",
-                ),
-            ]
-        )
-        self.root.ids.data_scr.add_widget(self.data_tables)
-'''
 
 if __name__ == '__main__':
     CommonApp().run()
